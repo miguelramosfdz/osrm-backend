18fbd451827f11e66dd0d531d596f5ccb20d96d1
Dennis Luxen <info@project-osrm.org>
2015-01-12 16:23:40 +0100

    fix move ctor to not pass data into nirvana

--- descriptors/descriptor_base.hpp
+++ descriptors/descriptor_base.hpp
@@ -34,6 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "../data_structures/phantom_node.hpp"
 #include "../Util/bearing.hpp"
 #include "../Util/cast.hpp"
+#include "../Util/simple_logger.hpp"
 #include "../typedefs.h"
 
 #include <osrm/json_container.hpp>
@@ -46,10 +47,10 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 struct DescriptorTable : public std::unordered_map<std::string, unsigned>
 {
     using super_class = std::unordered_map<std::string, unsigned>;
-    DescriptorTable(super_class &&map)
+    DescriptorTable(super_class &&map) : super_class(std::forward<super_class>(map))
     {
-        super_class(std::forward<super_class>(map));
     }
+
     unsigned get_id(const std::string &key) const
     {
         const auto iter = find(key);
